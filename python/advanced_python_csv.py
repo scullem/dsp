3c048b1ee4b33d8627001893334643982015e825
scullem <scullem@yahoo.com>
2015-12-27 14:44:41 -0500

    Update advanced_python_csv.py

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,16 @@
-PLACE YOUR CODE HERE
+import csv
+
+faculty = csv.DictReader(open('/Users/sarah/ds/metis/prework/dsp/python/faculty.csv'))
+
+def get_emails(dict):
+	email_address = []
+	for row in dict:
+		email_address.append(row[" email"])
+	return email_address
+
+emails = get_emails(faculty)
+
+f = open('emails.csv', 'w')
+for item in emails:
+    f.write(item + "\n")
+f.close()
